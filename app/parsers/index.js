615951206be2767a94776bbc8f1c433a0410528a
TimmyCarbone <timothy@timmycarbone.com>
2018-12-11 13:20:19 +0100

    Automatically require parsers

--- app/parsers/index.js
+++ app/parsers/index.js
@@ -1,3 +1,6 @@
-module.exports = {
-  parseRuntime: require('./runtime')
-};
+require('file-system').readdirSync(__dirname + '/').forEach(function(file) {
+  if (file.match(/\.js$/) !== null && file !== 'index.js') {
+    var name = file.replace('.js', '');
+    module.exports[name] = require('./' + file);
+  }
+});
