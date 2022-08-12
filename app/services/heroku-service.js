6f3e9ae6f700a4ea8ed0ff600878ae1d0cd722d8
Benjamin HEROULT <benj.97.am@gmail.com>
2022-08-12 15:26:12 +0200

    ADD winston

--- app/services/heroku-service.js
+++ app/services/heroku-service.js
@@ -1,13 +1,17 @@
 const axios = require('axios')
+const winston = require('winston')
 
 const { herokuApiUrl, herokuAppNameManaged, herokuToken } = require('../../configuration/index')
 
 module.exports = {
   restartDynos: () => {
+    winston.info('Restarting dynos on app', herokuAppNameManaged)
+
     const headers = {
       Authorization: `Bearer ${herokuToken}`,
     }
     const path = `/apps/${herokuAppNameManaged}/dynos`
+    console.log('axios', axios)
 
     return axios
       .delete(path, {
@@ -16,10 +20,12 @@ module.exports = {
       })
       .then(result => {
         console.log('restartDynos result', result.data)
+        winston.info('Dynos restarted successfully')
         return result.data
       })
       .catch(error => {
         console.log('restartDynos error', error)
+        winston.error('Failed to restart dynos', error)
         throw error
       })
   }
